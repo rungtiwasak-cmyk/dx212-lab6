1fb547f048eb8143d61212981e0a72fe4d03af16
Rungtiwa-Sakda <rungtiwa.sak@dome.tu.ac.th>
2026-09-16 10:06:34 +0700

    Initial commit

---
+++ Downloads/dx212/Lap 06/basic-js.js
@@ -0,0 +1,46 @@
+let name = "Tawan";
+let age = 20;
+let graduated = true;
+let gpa = 3.75;
+
+let student1 = {
+    name : "Manee",
+    age : 19,
+    graduated : false,
+    gpa : 2.65,
+};
+
+let student2 = {
+    name : "name",
+    age : age ,
+    graduated : graduated,
+    gpa : gpa ,
+};
+console.log(student1.name);
+console.log(student2);
+
+let grades = ["A","B","C","D","F"];
+let scores = [90,80,70,60,50];
+let students = [student1,student2];
+
+console.log (students[1].gpa);
+
+function calculateGrade(score){
+    if (score >= 90) {
+        return "A";
+    }else if (score >= 80) {
+        return "B";
+    }else if (score >= 70) {
+        return "C";
+    }else if (score >= 60) {
+        return "D";
+    }else if (score >= 50) {
+        return "F";
+    }
+}
+
+console.log(calculateGrade(90));
+
+for(let i =0; i<scores.length; i++) {
+    let grade = calculateGrade (score[i]);
+}
